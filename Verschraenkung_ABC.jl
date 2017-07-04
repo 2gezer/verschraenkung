bbb62a1d4bf111a18c297b8352a37f9ca17500be
2gezer <2gezer@web.de>
2017-07-04 13:42:31 +0200

    reduzierte D korrigiert

--- Verschraenkung_ABC.jl
+++ Verschraenkung_ABC.jl
@@ -1,3 +1,4 @@
+
 #Teilchenanzahl
 N=3
 # bezeichne Spins
@@ -8,26 +9,65 @@ ud = 01 #updown
 du = 10 #downup
 dd = 11 #downdown
 #erstelle Array für die Basisvektoren von System A
-A=[u d]
-a= size(A,2)
-#erstelle Array für Basisvektoren von System B
-BC= [u u u d d u d d ]
-b= size(BC,2)
+
+#Frei gewählter Zustand für A und B
+A= (1,1)
+a= size(A,1)
+
+BC= (0,1,0,0)
+b= size(BC,1)
+
 #Zustandsraum 2^N.... wie implentieren?
 2^N
-
-#Allgemeine Matrix mit allen möglichen Zuständen von A und B
+# #Teilchenanzahl A
+# N=8
+# N_a = N-rand(1:N)
+# N_b = N- N_a
+#Matrix aus den Zuständen von A und B
 Psi= zeros(a,b)
-
-Psi[1,1]=A[1]*B[2]
 
 for i in 1:a
   for j in 1:b
-    Psi[i,j] = A[i]*B[j]
+    Psi[i,j] = (A[i])*BC[j]
+
   end
 end
 
-#Freigewählter zustand
+#Normiere
+Psi = Psi/norm(Psi)
 
-Psi[1,2]
-Psi[2,2]
+#Berechne die reduzierte Dichtematrix mit ρ = ∑ ψ_ij*ψ_i'j
+ρ = zeros(a,a)
+for i in 1:size(Psi,1)
+  for k in 1:size(Psi,1)
+    for j in 1:size(Psi,2)
+      ρ[i,k]+= sum(Psi[i,j]*Psi[k,j])
+      #println(ρ) #markdown julia docs.
+    end
+  end
+end
+
+#Berechne Eigenwerte
+w= eigvals(ρ)
+v= zeros(w)
+for i in 1:length(w)
+  if w[i] <=0
+    v[i]=0
+    else
+      v[i]= (w[i]*log(w[i]))
+        end
+end
+if sum(v) ==0
+  V=0
+else
+V= -sum(v)
+end
+return println( "Anzahl aller Teilchen: ", sum(A)+sum(BC), "\n","\n",
+"Anzahl der Plätze in System A: ", A,"\n",
+"\n", "Anzahl der Plätze in System B : ",BC ,"\n",
+"Zustandsmatrix M: \n", Psi, "\n","\n",
+"Die reduzierte Dichtematrix des Systems A ubd B ist: \n", ρ, "\n","\n",
+"Eigenwerte: ", w, "\n", "\n",
+"Die Verschränkung des zufälligen Zustands beträgt: \n","\n",V)
+
+#Berechne Verschränkung
