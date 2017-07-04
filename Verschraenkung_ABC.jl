f884c9ba6147395e4e7e0ed0fa79939c8edb82d9
2gezer <2gezer@web.de>
2017-07-04 17:37:27 +0200

    fixed Matrix.jl

--- Verschraenkung_ABC.jl
+++ Verschraenkung_ABC.jl
@@ -1,73 +1,69 @@
 
 #Teilchenanzahl
-N=3
-# bezeichne Spins
-u=0 #up
-d=1 #down
-uu = 00 #upup
-ud = 01 #updown
-du = 10 #downup
-dd = 11 #downdown
+
+# # bezeichne Spins, wie?
+# u=0 #up
+# d=1 #down
+# uu = 00 #upup
+# ud = 01 #updown
+# du = 10 #downup
+# dd = 11 #downdown
 #erstelle Array für die Basisvektoren von System A
 
-#Frei gewählter Zustand für A und B
-A= (1,1)
-a= size(A,1)
+#Gebe Zustand durch manuelles Eingeben von Spinverteilung in A und B ein.
+function redMat(A,BC)
+  #Dimension von A
+  a= size(A,1)
+  #dimension von B
+  b= size(BC,1)
 
-BC= (0,1,0,0)
-b= size(BC,1)
+  #Zustandsraum 2^N.... wie implentieren?
+#  Psi = rand(0:1,(2^a,2^b)
 
-#Zustandsraum 2^N.... wie implentieren?
-2^N
-# #Teilchenanzahl A
-# N=8
-# N_a = N-rand(1:N)
-# N_b = N- N_a
-#Matrix aus den Zuständen von A und B
-Psi= zeros(a,b)
-
-for i in 1:a
-  for j in 1:b
-    Psi[i,j] = (A[i])*BC[j]
-
-  end
-end
-
-#Normiere
-Psi = Psi/norm(Psi)
-
-#Berechne die reduzierte Dichtematrix mit ρ = ∑ ψ_ij*ψ_i'j
-ρ = zeros(a,a)
-for i in 1:size(Psi,1)
-  for k in 1:size(Psi,1)
-    for j in 1:size(Psi,2)
-      ρ[i,k]+= sum(Psi[i,j]*Psi[k,j])
-      #println(ρ) #markdown julia docs.
+  Psi= zeros(a,b)
+  for i in 1:a
+    for j in 1:b
+      Psi[i,j] = (A[i])*BC[j]
     end
   end
-end
 
-#Berechne Eigenwerte
-w= eigvals(ρ)
-v= zeros(w)
-for i in 1:length(w)
-  if w[i] <=0
-    v[i]=0
+  #Normiere
+  Psi = Psi/norm(Psi)
+
+  #Berechne die reduzierte Dichtematrix mit ρ = ∑ ψ_ij*ψ_i'j
+  ρ = zeros(a,a)
+  for i in 1:size(Psi,1)
+    for k in 1:size(Psi,1)
+      for j in 1:size(Psi,2)
+        ρ[i,k]+= sum(Psi[i,j]*Psi[k,j]) #+ da, sonst ρ[i,k] zu Null überschrieben wird
+        #println(ρ) #markdown julia docs.
+      end
+    end
+  end
+
+  #Berechne Eigenwerte
+  w= eigvals(ρ)
+  v= zeros(w)
+  for i in 1:length(w)
+    if w[i] <=0
+      v[i]=0
     else
       v[i]= (w[i]*log(w[i]))
-        end
+    end
+  end
+  # Voraussetzung: Summe der Eigenwerte=1
+  if 1.1< sum(w) < 0.9
+      V=0
+      println("Keine Verschränkung, da Summe der Eigenwerte ≠ 1", sum(w), w)
+    else
+      V= -sum(v)
+  return println("Anzahl der Plätze: ", sum(A)+sum(BC), "\n","\n",
+  "Spinverteilung ausgehend von Anfangszustand in  A: ", A,"\n",
+  "\n", "Spinverteilung ausgehend von Anfangszustand in B : ",BC ,"\n","\n",
+  "normierte Zustandsmatrix Psi : \n", Psi, "\n","\n",
+  "Die reduzierte Dichtematrix des Systems A ubd B ist: \n", ρ, "\n","\n",
+  "Eigenwerte: ", w, "\n", "\n",
+  "Die Verschränkung des zufälligen Zustands beträgt: \n","\n",V)
 end
-if sum(v) ==0
-  V=0
-else
-V= -sum(v)
 end
-return println( "Anzahl aller Teilchen: ", sum(A)+sum(BC), "\n","\n",
-"Anzahl der Plätze in System A: ", A,"\n",
-"\n", "Anzahl der Plätze in System B : ",BC ,"\n",
-"Zustandsmatrix M: \n", Psi, "\n","\n",
-"Die reduzierte Dichtematrix des Systems A ubd B ist: \n", ρ, "\n","\n",
-"Eigenwerte: ", w, "\n", "\n",
-"Die Verschränkung des zufälligen Zustands beträgt: \n","\n",V)
-
-#Berechne Verschränkung
+  #Berechne Verschränkung
