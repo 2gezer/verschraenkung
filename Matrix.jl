4d87352aee57be10f95c2f3f62107160da224229
2gezer <2gezer@web.de>
2017-07-06 19:00:58 +0200

    überarbeitet

--- Matrix.jl
+++ Matrix.jl
@@ -1,46 +1,59 @@
 
 function Matrix(P)
+
+  ##zeros(Int8, 2^a, 2^b)??
+
   #Platzanzahl
   a = rand(1:(P-1))
   b = (P-a)
 
-  #erstelle zufälligen Zustand Psi mit zeilen von Psi == dim A , Spalten von Psi == dim B)
-  M= rand(0:1,(2^a,2^b))
+  #erstelle zufälligen Zustand Psi mit Zeilen von Psi == dim 2^A , Spalten von Psi == dim 2^B)
+  Psi= rand(0:1,2^a,2^b)
 
-  #Normieren: |a|^2+|b|^2 =1
-  M= M/norm(M)
+  #Normiere: |a|^2+|b|^2 =1
+  Psi= Psi/norm(Psi)
+
   #Berechne die reduzierte Dichtematrix mit ρ = ∑ ψ_ij*ψ_i'j
-  ρ = zeros(a,a)
+  ρ = zeros(2^a,2^a)
   for i in 1:size(Psi,1)
     for k in 1:size(Psi,1)
       for j in 1:size(Psi,2)
-        ρ[i,k]+= sum(Psi[i,j]*Psi[k,j]) #+ da, sonst ρ[i,k] zu Null überschrieben wird
-        #println(ρ) #markdown julia docs.
+        ρ[i,k]+= sum(Psi[i,j]*Psi[k,j])   #println(ρ) #markdown julia docs.
       end
     end
   end
+
   #Berechne Eigenwerte
   w= eigvals(ρ)
-  v= zeros(w)
+
+  #deklariere Verschränkungsvariable
+  V::Float32 =0.0
+
+  #Summe der Produkte der Eigenwerte mit ihren logarithmierten Werten
   for i in 1:length(w)
-    if w[i] < 0.00000000000001
-      v[i]=0
+    if abs(w[i]) < 1.0e-10
+      w[i] = 0.0
     else
-      v[i]= (w[i]*log(w[i]))
+      V += (w[i]*log(w[i]))
+      #  throw(w) Exception wenn?
     end
   end
-  #Berechne Verschränkung
-  if sum(v) ==0
-    V=0
-    println("Nicht verschränkt")
-  else
-    V= -sum(v)
-  end
-  return println( "Anzahl aller Teilchen: ", P, "\n","\n",
-  "Anzahl der Plätze in System A: ", a,"\n",
-  "\n", "Anzahl der Plätze in System B : ",b ,"\n",
-  "Zustandsmatrix M: \n", M, "\n","\n",
-  "Die reduzierte Dichtematrix des Systems A ubd B ist: \n", ρ, "\n","\n",
-  "Eigenwerte: ", w, "\n", "\n",
-  "Die Verschränkung des zufälligen Zustands beträgt: \n","\n",V)
+
+
+#Berechne Verschränkung
+
+# ein Zustand ist verschränkt, wenn die
+if  abs(V) < 1.0e-10
+  println("∑",w, "-> w*ln(w)=0 -> Nicht verschränkt","\n", "\n")
+else
+
+return println( "Anzahl aller Teilchen: ", P, "\n","\n",
+"Anzahl der Plätze in System A: ", a,"\n",
+"\n", "Anzahl der Plätze in System B : ",b ,"\n", "\n",
+"Zustandsmatrix Psi: \n", Psi, "\n","\n",
+"Die reduzierte Dichtematrix des System A ubd B ist: \n", ρ, "\n","\n",
+"Eigenwerte: ", w, "\n", "\n",
+"Die Verschränkung des zufälligen Zustands beträgt: \n","\n",-V)
+end
+return Psi,ρ,w
 end
